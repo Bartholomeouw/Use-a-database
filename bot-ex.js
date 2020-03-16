1f7d2ae8c7c8b2856a1078f1194a1ca1b482da33
Zerio <43386412+ZerioDev@users.noreply.github.com>
2020-03-16 14:34:09 +0100

    Add files via upload

---
+++ bot-ex.js
@@ -0,0 +1,19 @@
+//Configuration
+
+module.exports = {
+
+    token: "NjM2Mjc3NDM5OTI4MjA1MzEy.XkL-GA.HO0EQutWbWoGQLUUaz_lA1sCcBE", //Token bot
+    prefix: "?", //Basic prefix bot
+
+    ownerID: "617498280452161538", //ID
+
+    lang: "fr", //(en, fr)
+
+    emojis: {
+
+      check: ":white_check_mark:",
+      error: ":no_entry_sign:"
+
+    },
+
+};
